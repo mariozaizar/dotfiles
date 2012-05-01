7f82915fc210c364d8479dec06b76aa77eb908c0
Mario Zaizar <mariozaizar@gmail.com>
2012-05-01 11:05:17 -0500

    Change the promt to have the date-time in the last row.

--- files/bash/promt.sh
+++ files/bash/promt.sh
@@ -120,15 +120,15 @@ function git_info {
 
 # Note: don't mess with other's promts. Don't use export PS1
 if [ "$color_prompt" = yes ]; then
-  line1='\n\[\e[1;34m\]\@ \[\e[1;33m\]\w\n'
-  line2='\[\e[1;36m\]$(ruby_version)$(git_info)$(vagrant_status)\n'
-  line3='\[\e[1;34m\]\W \[\e[1;33m\]\$ \[\e[0;37m\]'
+  line1='\n\[\e[1;33m\]\w'
+  line2='\n\[\e[1;36m\]$(ruby_version)$(git_info)$(vagrant_status)'
+  line3='\n\[\e[1;34m\](\@) \[\e[1;34m\]\W \[\e[1;33m\]\$ \[\e[0;37m\]'
 
   PS1="${line1}${line2}${line3}"
 else
-  line1='\n\@ \w\n'
-  line2='$(ruby_version)$(git_info)$(vagrant_status)\n'
-  line3='\W \$ '
+  line1='\n\w'
+  line2='\n$(ruby_version)$(git_info)$(vagrant_status)'
+  line3='\n(\@) \W \$ '
 
   PS1="${line1}${line2}${line3}"
 fi
