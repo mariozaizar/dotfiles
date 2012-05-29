b187405c0063d51212f045510ac2fb95f8981e1c
Mario Zaizar <mariozaizar@gmail.com>
2012-05-29 16:45:38 -0500

    Increase the garbage collector limits for Ruby. Improve the performance.

--- files/bash/config.sh
+++ files/bash/config.sh
@@ -32,5 +32,12 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 # But WARNING, setting DYLD_LIBRARY_PATH can break dynamic linking.
 # export export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/
 
-# Personal
+# Make sublime your default editor
 export EDITOR="subl -w"
+
+# Increase the Garbage Collector limits for Ruby: http://goo.gl/IP8Jj
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
