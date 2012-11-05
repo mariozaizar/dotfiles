2cf8610fb50404b8b207a7aa28677b091a25e170
Mario Zaizar <mariozaizar@gmail.com>
2012-11-05 16:45:34 -0600

    Let's make the promt more like ZSH

--- files/bash/prompt.sh
+++ files/bash/prompt.sh
@@ -128,15 +128,15 @@ function git_info {
 #  Gris Claro  0;37     Blanco        1;37
 
 if [ "$color_prompt" = yes ]; then
-  line1='\n\[\e[1;36m\]\@ \[\e[1;35m\]\w\[\e[1;37m\]'
+  line1='\n\[\e[1;36m\]\@ \[\e[1;35m\]\w\[\e[1;37m\] at \[\e[1;36m\]\h'
   line2='\n\[\e[1;36m\]git: \[\e[1;35m\]$(git_branch), \[\e[1;36m\]ruby: \[\e[1;30m\]$(ruby_version), \[\e[1;36m\]vagrant: \[\e[1;30m\]$(vagrant_status)\[\e[1;37m\]'
-  line3='\n\[\e[1;36m\]\h \[\e[1;37m\]\$ '
+  line3='\n\[\e[1;37m\]→ '
 
   PS1="${line1}${line2}${line3}"
 else
-  line1='\n\@ \w'
+  line1='\n\@ \w at \h'
   line2='\ngit: $(git_branch), ruby: $(ruby_version), vagrant: $(vagrant_status)'
-  line3='\n@\h \$ '
+  line3='\n@→ '
 
   PS1="${line1}${line2}${line3}"
 fi
