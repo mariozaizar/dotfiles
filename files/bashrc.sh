725297b826c9b1982b3af95bedba8247cfaa942a
Mario Zaizar <mariozaizar@gmail.com>
2012-11-30 16:07:57 -0600

    Start using .personal to store personal settings.

--- files/bashrc.sh
+++ files/bashrc.sh
@@ -3,7 +3,7 @@ source ~/.bash/prompt
 source ~/.bash/aliases
 source ~/.bash/completion
 
-# Use .localrc for settings specific to one system
-if [ -f ~/.localrc ]; then
-  source ~/.localrc
+# Personal settings (should not overrided)
+if [ -f ~/.personal ]; then
+  source ~/.personal;
 fi
