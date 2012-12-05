b26576140580fd3ec4ae17db2e1869c6df66c158
Mario Zaizar <mariozaizar@gmail.com>
2012-12-05 16:35:15 -0600

    Addding more rvm set values.

--- files/rvmrc.sh
+++ files/rvmrc.sh
@@ -1,8 +1,10 @@
 export rvm_pretty_print_flag=1
+export rvm_trust_rvmrcs_flag=1
+export rvm_project_rvmrc=1
+
 ###############################################################################
 export rvm_path="<%= HOME_DIR %>/.rvm"
 ###############################################################################
 
 # 64 bit architecture for Mac OSX > 10.6
 rvm_archflags="-arch x86_64"
-rvm_project_rvmrc=1
